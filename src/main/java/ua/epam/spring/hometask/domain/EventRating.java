d51c8cd88cba8ad1807afbf26fbc1bb3ef59100b
Sergei Malgin <Sergei_Malgin@epam.com>
2017-04-28 16:35:25 +0300

    countries.xsd added

--- src/main/java/ua/epam/spring/hometask/domain/EventRating.java
+++ src/main/java/ua/epam/spring/hometask/domain/EventRating.java
@@ -1,14 +1,15 @@
 package ua.epam.spring.hometask.domain;
 
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
 /**
  * @author Yuriy_Tkach
  */
+@XmlEnum
+@XmlType
 public enum EventRating {
-
     LOW,
-
     MID,
-
     HIGH;
-
 }
